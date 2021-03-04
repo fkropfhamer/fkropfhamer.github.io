5664066cf70b5b8054f8e24a03713b38d05d2e8f
fkropfhamer <fabian.kropfhamer@sblum.de>
2021-03-04 11:36:53 +0100

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>fkropfhamer.</h1>
     </div>
